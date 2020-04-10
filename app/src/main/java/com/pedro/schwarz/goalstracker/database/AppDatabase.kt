8ff6597c1debdb46852befeb0e1e4c2fafdd9332
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2020-04-09 22:55:30 -0300

    Database created

--- app/src/main/java/com/pedro/schwarz/goalstracker/database/AppDatabase.kt
+++ app/src/main/java/com/pedro/schwarz/goalstracker/database/AppDatabase.kt
@@ -1,2 +1,11 @@
 package com.pedro.schwarz.goalstracker.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.pedro.schwarz.goalstracker.database.dao.GoalDAO
+import com.pedro.schwarz.goalstracker.model.Goal
+
+@Database(entities = [Goal::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getGoalDAO(): GoalDAO
+}
--- app/src/main/java/com/pedro/schwarz/goalstracker/di/modules/AppModules.kt
+++ app/src/main/java/com/pedro/schwarz/goalstracker/di/modules/AppModules.kt
@@ -1,13 +1,21 @@
 package com.pedro.schwarz.goalstracker.di.modules
 
+import androidx.room.Room
+import com.pedro.schwarz.goalstracker.database.AppDatabase
+import com.pedro.schwarz.goalstracker.database.dao.GoalDAO
 import com.pedro.schwarz.goalstracker.repository.AuthRepository
 import com.pedro.schwarz.goalstracker.ui.viewmodel.AuthViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val databaseModules = module { }
+private const val DATABASE_NAME = "goals_tracker.db"
+
+val databaseModules = module {
+    single { Room.databaseBuilder(get(), AppDatabase::class.java, DATABASE_NAME).build() }
+}
 
 val daoModules = module {
+    single<GoalDAO> { get<AppDatabase>().getGoalDAO() }
     single<AuthRepository> { AuthRepository() }
 }
 
